36ab1305563350e1edd2f1b4502894557fb660bf
Asadi <imtiazasad74@gmail.com>
2023-03-02 23:40:31 +0000

    JS

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
 function Solve(val){
-    var v = document.getElementById('res')
+    var v = document.getElementById('res');
     v.value += val;
+}
+function Result(){
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
 }
